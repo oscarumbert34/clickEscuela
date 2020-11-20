38c4853a1844a3487d4d0016b42630f6a938478f
umbert oscar <oscar.umbert@gmail.com>
2020-11-20 18:08:55 -0300

    se agrega tabla material

--- src/app/components/teacher/courses/courses.component.ts
+++ src/app/components/teacher/courses/courses.component.ts
@@ -23,15 +23,15 @@ export class CoursesComponent implements OnInit {
     this.dashBoardsProperties.push(new Dashboardproperties('Asistencia de alumnos', '20/20', 'icon-card-attendance', null, null));
     this.dashBoardsProperties.push(new Dashboardproperties('Cantidad de aprobados', '15/20', 'icon-card-approved', null, examen));
 
-    this.dashBoardsProperties.push(new Dashboardproperties('Trabajo Practico', '80%', 'icon-card-homework', null, trabajoPractico));
-    this.dashBoardsProperties.push(new Dashboardproperties('Tareas', '13/20', 'icon-card', null ,tareas));
-    this.dashBoardsProperties.push(new Dashboardproperties('Asistencia de alumnos', '20/20', 'icon-card-attendance', null, null));
-    this.dashBoardsProperties.push(new Dashboardproperties('Cantidad de aprobados', '15/20', 'icon-card-approved', null, examen));
+    this.dashBoardsProperties1.push(new Dashboardproperties('Trabajo Practico', '80%', 'icon-card-homework', null, trabajoPractico));
+    this.dashBoardsProperties1.push(new Dashboardproperties('Tareas', '13/20', 'icon-card', null ,tareas));
+    this.dashBoardsProperties1.push(new Dashboardproperties('Asistencia de alumnos', '20/20', 'icon-card-attendance', null, null));
+    this.dashBoardsProperties1.push(new Dashboardproperties('Cantidad de aprobados', '15/20', 'icon-card-approved', null, examen));
 
-    this.dashBoardsProperties.push(new Dashboardproperties('Trabajo Practico', '80%', 'icon-card-homework', null, trabajoPractico));
-    this.dashBoardsProperties.push(new Dashboardproperties('Tareas', '13/20', 'icon-card', null ,tareas));
-    this.dashBoardsProperties.push(new Dashboardproperties('Asistencia de alumnos', '20/20', 'icon-card-attendance', null, null));
-    this.dashBoardsProperties.push(new Dashboardproperties('Cantidad de aprobados', '15/20', 'icon-card-approved', null, examen));
+    this.dashBoardsProperties2.push(new Dashboardproperties('Trabajo Practico', '80%', 'icon-card-homework', null, trabajoPractico));
+    this.dashBoardsProperties2.push(new Dashboardproperties('Tareas', '13/20', 'icon-card', null ,tareas));
+    this.dashBoardsProperties2.push(new Dashboardproperties('Asistencia de alumnos', '20/20', 'icon-card-attendance', null, null));
+    this.dashBoardsProperties2.push(new Dashboardproperties('Cantidad de aprobados', '15/20', 'icon-card-approved', null, examen));
 
   }
 
--- src/app/models/student.ts
+++ src/app/models/student.ts
@@ -1,5 +1,5 @@
 export class Student{
-     id: string;
+     public id: string;
      name: string;
      surname: string;
      bornDate: string;
