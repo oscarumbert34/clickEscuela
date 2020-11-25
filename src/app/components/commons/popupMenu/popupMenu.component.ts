704281435e61bee54f41d1b56a4870a94a34cfc2
nicolencinas <nicolencinas@hotmail.com>
2020-11-25 17:17:41 -0300

    subiendo cambios en el menu de notas

--- src/app/components/commons/popupMenu/popupMenu.component.ts
+++ src/app/components/commons/popupMenu/popupMenu.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-popupMenu',
   templateUrl: './popupMenu.component.html',
-  styleUrls: ['./popupMenu.component.css']
+  styleUrls: ['./popupMenu.component.scss']
 })
 export class PopupMenuComponent implements OnInit {
 
