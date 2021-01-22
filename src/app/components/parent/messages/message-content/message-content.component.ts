768f96c69b12cf734c5b170f8d527c9ee47dd375
nicolencinas <nicolencinas@hotmail.com>
2021-01-21 21:41:26 -0300

    subiendo cambios graficos

--- src/app/components/parent/messages/message-content/message-content.component.ts
+++ src/app/components/parent/messages/message-content/message-content.component.ts
@@ -1,5 +1,7 @@
+import { element } from 'protractor';
+import { style } from '@angular/animations';
 import { MessagesService } from './../../../../services/messages.service';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewChildren, ElementRef, QueryList } from '@angular/core';
 import { Message } from 'src/app/models/Message';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
@@ -10,18 +12,30 @@ import { MatTableDataSource } from '@angular/material/table';
   templateUrl: './message-content.component.html',
   styleUrls: ['./message-content.component.scss']
 })
-export class MessageContentComponent implements OnInit {
-
+export class MessageContentComponent implements OnInit 
+{
+  colors: string[]
+  selectedColor:string;
   messageList:Message[];
   displayedColumns: string[];
   dataSource: any;
   
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChildren('userAvatar') avatars: QueryList<HTMLDivElement>;
+
+
+  root;
+
   
   constructor(private messageService: MessagesService)
   {
     this.messageList=this.messageService.messageList
+    this.colors=['#049dd975','#14a6479c','#f2c84b7c','#f24e2975','#f2b9b37e'];
+    this.selectedColor=this.randomColor()
+
+    this.root=document.documentElement;
+   
   }
 
   ngOnInit() 
@@ -37,7 +51,47 @@ export class MessageContentComponent implements OnInit {
     this.dataSource.sort = this.sort;
 
 
+
     
   }
+
+  
+
+  randomColor()
+  {
+    let num= Math.floor(Math.random() * (4 - 0)) + 0;
+    return this.colors[num]
+
+  }
+
+  showChildrens()
+  {
+    this.avatars.forEach((element,index) =>
+     console.log(element ) 
+
+    // element.nativeElement
+    
+
+
+     )
+
+  }
+
+  setColor(i)
+  {
+    this.root.style.setProperty('--background-header',this.colors[i])
+    
+    
+  }
+
+  getColor(i)
+  {
+    
+    return(this.colors[i])
+
+  }
+
+
+  
 
 }
