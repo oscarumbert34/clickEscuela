d1ea52a24a11f736d0e460aa89fb3d8f37f48744
nicolencinas <nicolencinas@hotmail.com>
2021-01-29 22:13:41 -0300

    subiendo con imagenes

--- src/app/models/Message.ts
+++ src/app/models/Message.ts
@@ -4,19 +4,21 @@ export class Message
     content:string;
     type:string;
     date:Date;
+    avatarImg:string
 
     constructor
     (
      sender:string,
      content:string,
      type:string,
-     date:Date
+     date:Date,avatarImg:string
     )
     {
         this.sender=sender
         this.content=content
         this.type=type
         this.date=date;
+        this.avatarImg=avatarImg
     }
 
 }
