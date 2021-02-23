3673507fb7c32a6914f60bc998c5574ccb0e35a9
nicolencinas <nicolencinas@hotmail.com>
2021-02-22 23:54:48 -0300

    subiendo cambios antes de crear componentet

--- src/app/components/student/library/library.component.ts
+++ src/app/components/student/library/library.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChildren, QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-library',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibraryComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  @ViewChildren('test') imageslist: QueryList<ElementRef>
+  images:ElementRef[];
+  
+  constructor() {
+    this.images=[]
   }
+  
+  ngOnInit()
+  { 
+    //this.startImageTransition(); 
+  }
+
+  
+
+
 
 }
