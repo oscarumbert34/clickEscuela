da1e28333f9c67c271403e12682c1cbc834c9a70
nicolencinas <nicolencinas@hotmail.com>
2021-02-24 21:43:37 -0300

    subiendo descarga directa

--- src/app/services/libraryService.service.ts
+++ src/app/services/libraryService.service.ts
@@ -16,7 +16,7 @@ constructor()
   this.filesList.push(new LibraryFile("Practica del lenguaje", "Santillana","Lengua",'http://localhost:4200/assets/pdf/prueba1.pdf?'))
   this.filesList.push(new LibraryFile("Inteligencia matematica", "Granica","Matematica",'http://localhost:4200/assets/pdf/prueba2.pdf?'))
   this.filesList.push(new LibraryFile("Biologia 3 ", "Santillana","Biologia",'http://localhost:4200/assets/pdf/prueba3.pdf?'))
-  this.filesList.push(new LibraryFile("Manual de javascript", "Miguel Angel Alvarez","Programacion",'http://localhost:4200/assets/pdf/manual.pdf?'))
+  this.filesList.push(new LibraryFile("Manual de javascript", "Miguel Angel Alvarez/ Manu Gutierrez ","Programacion",'http://localhost:4200/assets/pdf/manual.pdf?'))
 
  }
 
