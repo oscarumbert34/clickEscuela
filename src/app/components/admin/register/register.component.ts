34b4dca794dd7cee2d84dad7c63264022046ac91
nicolencinas <nicolencinas@hotmail.com>
2021-04-08 17:42:44 -0300

    subiendo cambios

--- src/app/components/admin/register/register.component.ts
+++ src/app/components/admin/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  @ViewChild('drawer', { static: true }) sidenav: MatSidenav;
+  blockDinamicActually = 'parent-asistance';
+  showHomeButton = false;
+
+
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() 
+  {
+    this.sidenav.open()
   }
+
+  changeBlock(newBlock: string) {
+    console.log(this.showHomeButton);
+    this.showHomeButton = newBlock != 'home' ? true : false;
+    this.blockDinamicActually = newBlock;
+    console.log(newBlock)
+  }
+
 
 }
