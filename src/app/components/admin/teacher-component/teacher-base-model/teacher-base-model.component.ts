36a561b51a684e0b11afcd42155c5662d53ad185
nicolencinas <nicolencinas@hotmail.com>
2021-04-16 16:12:06 -0300

    subiendo alta de docente

---
+++ src/app/components/admin/teacher-component/teacher-base-model/teacher-base-model.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-base-model',
+  templateUrl: './teacher-base-model.component.html',
+  styleUrls: ['./teacher-base-model.component.css']
+})
+export class TeacherBaseModelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
