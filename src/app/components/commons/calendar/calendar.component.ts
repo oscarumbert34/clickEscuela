244ca3e924661ceb47d1e2c3fdf9fd741363b5f1
nicolencinas <nicolencinas@hotmail.com>
2021-05-07 21:39:08 -0300

    subiendo calendar Fill

--- src/app/components/commons/calendar/calendar.component.ts
+++ src/app/components/commons/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-calendar',
@@ -6,10 +6,148 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent implements OnInit {
+  weeks: any[]
 
-  constructor() { }
+  @ViewChildren('day') calendarDays: QueryList<ElementRef>
+
+  constructor() {
+    this.weeks = []
+
+    let count = 1;
+
+    for (let i = 1; i <= 6; i++) {
+      let week = []
+      for (let j = 1; j <= 7; j++) {
+        week.push("day" + count);
+        count++
+      }
+      this.weeks.push(week)
+    }
+
+    console.log(this.weeks)
+  }
+
+
 
   ngOnInit() {
+
+
   }
+
+  ngAfterViewInit() {
+    console.log(this.calendarDays)
+    this.llenarCalendario(0, 0)
+    document.getElementById("day8").innerHTML == "Hola"
+  }
+
+
+  llenarCalendario(anio, mes) {
+
+
+    var monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    var displacementdays = [0, 1, 2, 3, 4, 5, 6];
+
+    var currentDate;
+    if (mes == 0 && anio == 0) {
+      currentDate = new Date();
+    } else {
+      currentDate = new Date(anio, mes, 1);
+    }
+
+    console.log(currentDate)
+
+
+    var mesnombre = monthNames[currentDate.getMonth()];
+
+    document.getElementById("mesnombre").innerHTML = mesnombre;
+    document.getElementById("numeroanio").innerHTML = currentDate.getFullYear();
+
+    var numberMonth = document.getElementById("numeromes");
+    var currentMonth = (currentDate.getMonth() + 1) + "";
+
+
+
+    var large = currentMonth.length;
+
+    var day = ("0" + currentDate.getDate()).slice(-2)
+    var month = ("0" + (currentDate.getMonth() + 1)).slice(-2)
+    var year = currentDate.getFullYear();
+
+    numberMonth.innerHTML = month;
+
+    var firstDay = new Date((currentDate.getMonth() + 1) + ' ' + '01' + ', ' + currentDate.getFullYear() + ' 12:00:00');
+
+
+
+    var displace = displacementdays[firstDay.getUTCDay()];
+    console.log(displace)
+
+    for (var i = 1; i <= 42; i++) {
+      var celd = document.getElementById("numberday" + i)
+      celd.innerHTML = "";
+    }
+
+    var dia = 1;
+    var totaldays = this.daysInMonth(currentDate.getMonth() + 1, currentDate.getFullYear());
+
+    for (var i = displace + 1; i <= totaldays + displace; i++) {
+      let id = 'numberday' + i
+      document.getElementById(id).innerHTML =dia+""
+       
+      dia++;
+    }
+  }
+
+
+  daysInMonth(mes, año) {
+    return new Date(año, mes, 0).getDate();
+  }
+
+
+
+
+  prevMonth() {
+    var month = parseInt(document.getElementById("numeromes").innerHTML) - 1;
+    var year = parseInt(document.getElementById("numeroanio").innerHTML);
+
+    if (month == 0) {
+      month = 11;
+      year--
+    }
+    else {
+      month--;
+    }
+
+    this.llenarCalendario(year, month);
+    // for (var i = 1; i <= 37; i++) {
+    //   document.getElementById("dia" + i).style.fill = "black"
+    //   document.getElementById("dia" + i).style.fontSize = "41.4646px"
+    // }
+
+
+
+  }
+
+  nextMonth() {
+    var month = parseInt(document.getElementById("numeromes").innerHTML) - 1;
+    var year = parseInt(document.getElementById("numeroanio").innerHTML);
+
+    if (month == 11) {
+      month = 0;
+      year++
+    }
+    else {
+      month++;
+    }
+
+    this.llenarCalendario(year, month);
+
+    // for (var i = 1; i <= 37; i++) {
+    //   document.getElementById("dia" + i).style.fill = "black"
+    //   document.getElementById("dia" + i).style.fontSize = "41.4646px"
+    // }
+  }
+
+
 
 }
