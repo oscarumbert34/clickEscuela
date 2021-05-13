9621f7fe39c967eeb593d2fb1d8153258c2d37f8
nicolencinas <nicolencinas@hotmail.com>
2021-05-12 23:48:27 -0300

    subiendo el eventDetail ajustado a pantalla

--- src/app/components/commons/eventDetail/eventDetail.component.ts
+++ src/app/components/commons/eventDetail/eventDetail.component.ts
@@ -1,5 +1,10 @@
+import { CalendarEvent } from './../../../models/calendar-event';
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import moment from 'moment';
+import { M } from '@angular/cdk/keycodes';
+
+
 
 @Component({
   selector: 'app-eventDetail',
@@ -8,11 +13,37 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class EventDetailComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<EventDetailComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
+  week: any = [
+    "Lunes",
+    "Martes",
+    "Miercoles",
+    "Jueves",
+    "Viernes",
+    "Sabado",
+    "Domingo"
+  ];
+
+  monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+
+  dayName:string;
+
+
+  constructor(public dialogRef: MatDialogRef<EventDetailComponent>, @Inject(MAT_DIALOG_DATA) public event: CalendarEvent) 
+  { 
+    let day=moment(event.$day).day()
+    let month=moment(event.$day).month()
+    let hour=moment(event.$day).format("HH:MM")
+    this.dayName=
+    this.week[day]+
+    ", "+moment(event.$day).format('DD')+
+    ' de '+this.monthNames[month]+" " 
+    +hour+" horas"
+  }
 
   ngOnInit() 
   {
-    console.log(this.data)
+    console.log(this.dayName)
   }
 
 }
