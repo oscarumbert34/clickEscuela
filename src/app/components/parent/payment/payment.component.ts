b0cf2a4945a55555d7efc7419ae0693886d170cb
nicolencinas <nicolencinas@hotmail.com>
2021-05-20 22:18:32 -0300

    cambiando el detalle de payments

--- src/app/components/parent/payment/payment.component.ts
+++ src/app/components/parent/payment/payment.component.ts
@@ -1,9 +1,13 @@
+import { studentService } from 'src/app/services/student.service';
+import { AccountService } from 'src/app/services/account.service';
+import { PaymentsDetailComponent } from 'src/app/components/commons/payments-detail/payments-detail.component';
 import { ConfirmDialogComponent } from 'src/app/components/commons/confirm-dialog/confirm-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { element } from 'protractor';
 import { PaymentService } from './../../../services/payment.service';
 import { Component, OnInit } from '@angular/core';
 import { Payment } from 'src/app/models/payment';
+import { Student } from 'src/app/models/student';
 
 @Component({
   selector: 'app-payment',
@@ -16,25 +20,28 @@ export class PaymentComponent implements OnInit {
   nextExpiration: Date;
   paymentList: Payment[];
   faultpayments: Payment[];
+  currentStudent:Student;
+  idStudent='1'
 
   paymentDebt = 0;
 
-  constructor(private paymentService: PaymentService, private dialog: MatDialog) {
+  constructor(private paymentService: PaymentService, private dialog: MatDialog,private accountService:AccountService, private studentsService:studentService) {
     this.currentDate = new Date()
     this.nextExpiration = new Date()
     this.nextExpiration.setFullYear(2021);
     this.nextExpiration.setDate(12)
     this.nextExpiration.setMonth(3)
-    this.paymentList = paymentService.paymentList
+    this.paymentList = accountService.accountsList.filter(a=>a.$titularId===this.idStudent)[0].$payments
+    this.currentStudent= studentsService.studentsList.filter(a=>a.id===this.idStudent)[0]
     this.faultpayments = []
 
   }
 
   getDetail() {
-    const dialogRef = this.dialog.open(ConfirmDialogComponent,
+    const dialogRef = this.dialog.open(PaymentsDetailComponent,
       {
-        data: this.faultpayments,
-        width: '60%',
+        data:{payment:this.paymentList,student:this.currentStudent},
+        width: '80vw',
         height: '300px'
       }
     )
