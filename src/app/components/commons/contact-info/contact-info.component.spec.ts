816772c3d31e575275861e75e8de95586e33510d
nicolencinas <nicolencinas@hotmail.com>
2021-07-07 23:57:07 -0300

    Limpiando Failures de test

--- src/app/components/commons/contact-info/contact-info.component.spec.ts
+++ src/app/components/commons/contact-info/contact-info.component.spec.ts
@@ -1,3 +1,4 @@
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
@@ -8,10 +9,24 @@ import { ContactInfoComponent } from './contact-info.component';
 describe('ContactInfoComponent', () => {
   let component: ContactInfoComponent;
   let fixture: ComponentFixture<ContactInfoComponent>;
+  const data = {
+    adress: 'some',
+    telephone: 'some',
+    email: 'some',
+    web: 'some',
+    celular: 'some',
+    whatsapp: 'some',
+    telegram: 'some',
+    linkedin: 'some'
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContactInfoComponent ]
+      declarations: [ ContactInfoComponent ],
+      providers: [
+        {provide: MatDialogRef, useValue: {}},
+        {provide: MAT_DIALOG_DATA, useValue: data}
+      ]
     })
     .compileComponents();
   }));
